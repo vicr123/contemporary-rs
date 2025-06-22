2b87f31427e6bb391f18d7bc9d3e9290088b0a08
Victor Tran <vicr12345@gmail.com>
2025-06-22 16:53:31 +1000

    Improve checkbox drawing

--- contemporary/src/components/checkbox.rs
+++ contemporary/src/components/checkbox.rs
@@ -2,9 +2,12 @@ use crate::styling::theme::Theme;
 use gpui::prelude::FluentBuilder;
 use gpui::MouseButton::Left;
 use gpui::{
-    div, px, App, Div, ElementId, InteractiveElement, IntoElement, ParentElement,
-    RenderOnce, SharedString, Stateful, StatefulInteractiveElement, Styled, Window,
+    div, px, quad, size, transparent_black, white, App, AvailableSpace, BorderStyle,
+    Bounds, Corners, Div, Element, ElementId, GlobalElementId,
+    InspectorElementId, InteractiveElement, IntoElement, LayoutId, ParentElement, Pixels,
+    Refineable, RenderOnce, SharedString, Stateful, StatefulInteractiveElement, Style, StyleRefinement, Styled, Window,
 };
+use std::panic::Location;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum CheckState {
@@ -22,8 +25,8 @@ pub struct CheckedChangeEvent {
 pub struct Checkbox {
     div: Stateful<Div>,
     check_state: CheckState,
-    label: Option<SharedString>,
     draw_as_radio: bool,
+    label: Option<SharedString>,
 
     checked_changed_handlers: Vec<Box<dyn Fn(&CheckedChangeEvent, &mut Window, &mut App)>>,
 }
@@ -32,8 +35,8 @@ pub fn checkbox(id: impl Into<ElementId>) -> Checkbox {
     Checkbox {
         div: div().id(id),
         check_state: CheckState::Off,
-        label: None,
         draw_as_radio: false,
+        label: None,
         checked_changed_handlers: Vec::new(),
     }
 }
@@ -42,8 +45,8 @@ pub fn radio_button(id: impl Into<ElementId>) -> Checkbox {
     Checkbox {
         div: div().id(id),
         check_state: CheckState::Off,
-        label: None,
         draw_as_radio: true,
+        label: None,
         checked_changed_handlers: Vec::new(),
     }
 }
@@ -88,21 +91,13 @@ impl RenderOnce for Checkbox {
 
         self.div
             .flex()
-            .gap(px(6.))
+            .items_center()
+            .gap(px(4.))
             .child(
-                div()
-                    .when_else(
-                        self.draw_as_radio,
-                        |div| div.rounded(px(300.)),
-                        |div| div.rounded(theme.border_radius),
-                    )
-                    .border(px(1.))
-                    .border_color(theme.foreground)
-                    .min_w(px(12.))
-                    .min_h(px(12.))
-                    .when(self.check_state == CheckState::On, |div| {
-                        div.bg(theme.foreground)
-                    }),
+                checkbox_box(self.check_state, self.draw_as_radio)
+                    .m(px(2.))
+                    .rounded(theme.border_radius)
+                    .text_color(theme.foreground),
             )
             .when_some(self.label, |div, label| div.child(label))
             .on_click(move |event, window, cx| {
@@ -122,5 +117,155 @@ impl RenderOnce for Checkbox {
                     fun(&event, window, cx)
                 }
             })
+    }
+}
+
+struct CheckboxBox {
+    check_state: CheckState,
+    draw_as_radio: bool,
+    style: StyleRefinement,
+}
+
+fn checkbox_box(check_state: CheckState, draw_as_radio: bool) -> CheckboxBox {
+    CheckboxBox {
+        check_state,
+        draw_as_radio,
+        style: StyleRefinement::default(),
+    }
+}
+
+impl IntoElement for CheckboxBox {
+    type Element = Self;
+
+    fn into_element(self) -> Self::Element {
+        self
+    }
+}
+
+impl Styled for CheckboxBox {
+    fn style(&mut self) -> &mut StyleRefinement {
+        &mut self.style
+    }
+}
+
+impl Element for CheckboxBox {
+    type RequestLayoutState = ();
+    type PrepaintState = ();
+
+    fn id(&self) -> Option<ElementId> {
+        Some("box".into())
+    }
+
+    fn source_location(&self) -> Option<&'static Location<'static>> {
+        None
+    }
+
+    fn request_layout(
+        &mut self,
+        _id: Option<&GlobalElementId>,
+        _inspector_id: Option<&InspectorElementId>,
+        window: &mut Window,
+        _cx: &mut App,
+    ) -> (LayoutId, Self::RequestLayoutState) {
+        let mut style = Style::default();
+        style.refine(&self.style);
+        let layout_id = window.request_measured_layout(style, |_, available_space, _, _| {
+            let mut dim = match available_space.width {
+                AvailableSpace::Definite(px) => px.0,
+                AvailableSpace::MinContent => f32::NAN,
+                AvailableSpace::MaxContent => f32::NAN,
+            }
+            .min(match available_space.height {
+                AvailableSpace::Definite(px) => px.0,
+                AvailableSpace::MinContent => f32::NAN,
+                AvailableSpace::MaxContent => f32::NAN,
+            });
+            if dim.is_nan() {
+                dim = 16.;
+            }
+            size(px(dim), px(dim))
+        });
+
+        (layout_id, ())
+    }
+
+    fn prepaint(
+        &mut self,
+        _id: Option<&GlobalElementId>,
+        _inspector_id: Option<&InspectorElementId>,
+        _bounds: Bounds<Pixels>,
+        _request_layout: &mut Self::RequestLayoutState,
+        _window: &mut Window,
+        _cx: &mut App,
+    ) -> Self::PrepaintState {
+        // noop
+    }
+
+    fn paint(
+        &mut self,
+        _id: Option<&GlobalElementId>,
+        _inspector_id: Option<&InspectorElementId>,
+        bounds: Bounds<Pixels>,
+        _request_layout: &mut Self::RequestLayoutState,
+        _prepaint: &mut Self::PrepaintState,
+        window: &mut Window,
+        _cx: &mut App,
+    ) {
+        let background = self
+            .style
+            .background
+            .clone()
+            .and_then(|background| background.color())
+            .unwrap_or(transparent_black().into());
+        let foreground = self
+            .style
+            .text
+            .clone()
+            .and_then(|text| text.color)
+            .unwrap_or(white().into());
+
+        if self.draw_as_radio {
+            window.paint_quad(quad(
+                bounds,
+                Corners::all(px(bounds.size.width.0 / 2.)),
+                background,
+                px(1.),
+                foreground,
+                BorderStyle::Solid,
+            ));
+
+            if self.check_state == CheckState::On {
+                window.paint_quad(quad(
+                    bounds,
+                    Corners::all(px(bounds.size.width.0 / 2.)),
+                    transparent_black(),
+                    px(bounds.size.width.0 / 4.),
+                    foreground,
+                    BorderStyle::Solid,
+                ));
+            }
+        } else {
+            let mut corners = Corners::default();
+            corners.refine(&self.style.corner_radii);
+            window.paint_quad(quad(
+                bounds,
+                corners.to_pixels(window.rem_size()),
+                background,
+                px(1.),
+                foreground,
+                BorderStyle::Solid,
+            ));
+
+            if self.check_state == CheckState::On {
+                window.paint_quad(quad(
+                    bounds,
+                    corners.to_pixels(window.rem_size()),
+                    foreground,
+                    px(1.),
+                    foreground,
+                    BorderStyle::Solid,
+                ));
+            }
+        }
     }
 }
