aae88b31085747f579576242783775bc00db3283
Victor Tran <vicr12345@gmail.com>
2025-08-14 00:23:29 +1000

    Mark tests as #[cfg(test)]

--- i18n/cntp_i18n_gen/src/tests/helpers.rs
+++ i18n/cntp_i18n_gen/src/tests/helpers.rs
@@ -1,18 +1,18 @@
-use crate::TrMacroVisitor;
-use icu::locale::Locale;
-use icu::plurals::PluralRules;
-use std::cell::RefCell;
-use std::path::PathBuf;
-use std::rc::Rc;
-use syn::parse_file;
-use syn::visit::Visit;
+#[cfg(test)]
+pub fn visit_string(string: &str, locale: &str) -> crate::TrMacroVisitor {
+    use icu::locale::Locale;
+    use icu::plurals::PluralRules;
+    use std::cell::RefCell;
+    use std::path::PathBuf;
+    use std::rc::Rc;
+    use syn::parse_file;
+    use syn::visit::Visit;
 
-pub fn visit_string(string: &str, locale: &str) -> TrMacroVisitor {
     let Ok(syntax) = parse_file(string) else {
         panic!("failed to parse source file");
     };
 
-    let mut visitor = TrMacroVisitor::new(
+    let mut visitor = crate::TrMacroVisitor::new(
         PluralRules::try_new(
             Locale::try_from_str(locale).expect("Invalid Locale").into(),
             Default::default(),
