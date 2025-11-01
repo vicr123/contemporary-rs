1e81e46d6b402480d4943946fcea18bb020f1a20
Victor Tran <vicr12345@gmail.com>
2025-11-02 10:10:37 +1100

    Implement actions on Linux

--- contemporary/src/platform_support/linux/notification.rs
+++ contemporary/src/platform_support/linux/notification.rs
@@ -1,6 +1,10 @@
-use crate::notification::{Notification, NotificationPriority, PostedNotification};
-use ashpd::desktop::notification::{NotificationProxy, Priority};
-use gpui::{App, AsyncApp};
+use crate::notification::{
+    Notification, NotificationActionEvent, NotificationPriority, PostedNotification,
+};
+use ashpd::desktop::notification::{Button, NotificationProxy, Priority};
+use gpui::{App, AsyncApp, BorrowAppContext, Global};
+use smol::stream::StreamExt;
+use std::collections::HashMap;
 use uuid::Uuid;
 
 struct LinuxPostedNotification {
@@ -34,16 +38,16 @@ impl PostedNotification for LinuxPostedNotification {
 
     fn replace(&self, notification: Notification, cx: &mut App) {
         let id_clone = self.id.clone();
+
+        let ashpd_notification = contemporary_notification_to_ashpd_notification(notification, cx);
+
         cx.spawn(async move |cx: &mut AsyncApp| {
             let Ok(notification_portal) = NotificationProxy::new().await else {
                 return;
             };
 
             let _ = notification_portal
-                .add_notification(
-                    id_clone.as_str(),
-                    contemporary_notification_to_ashpd_notification(notification),
-                )
+                .add_notification(id_clone.as_str(), ashpd_notification)
                 .await;
         })
         .detach();
@@ -53,6 +57,11 @@ impl PostedNotification for LinuxPostedNotification {
 pub fn post_notification(notification: Notification, cx: &mut App) -> Box<dyn PostedNotification> {
     let id = Uuid::new_v4().to_string();
 
+    let body = notification.body.clone();
+    let summary = notification.summary.clone();
+
+    let ashpd_notification = contemporary_notification_to_ashpd_notification(notification, cx);
+
     let id_clone = id.clone();
     cx.spawn(async move |cx: &mut AsyncApp| {
         // TODO: If the portal is not available, fall back to org.freedesktop.Notifications
@@ -61,28 +70,86 @@ pub fn post_notification(notification: Notification, cx: &mut App) -> Box<dyn Po
         };
 
         let _ = notification_portal
-            .add_notification(
-                id_clone.as_str(),
-                contemporary_notification_to_ashpd_notification(notification),
-            )
+            .add_notification(id_clone.as_str(), ashpd_notification)
             .await;
     })
     .detach();
 
-    Box::new(LinuxPostedNotification { id })
+    Box::new(LinuxPostedNotification { id, body, summary })
 }
 
 fn contemporary_notification_to_ashpd_notification(
     notification: Notification,
+    cx: &mut App,
 ) -> ashpd::desktop::notification::Notification {
-    ashpd::desktop::notification::Notification::new(
-        notification.summary.unwrap_or_default().as_str(),
-    )
-    .body(notification.body.unwrap_or_default().as_str())
-    .priority(match notification.priority {
-        NotificationPriority::Low => Priority::Low,
-        NotificationPriority::Normal => Priority::Normal,
-        NotificationPriority::High => Priority::High,
-        NotificationPriority::Urgent => Priority::Urgent,
+    cx.update_global::<LinuxNotificationGlobal, _>(|linux_notification_global, cx| {
+        let mut ashpd_notification = ashpd::desktop::notification::Notification::new(
+            notification.summary.unwrap_or_default().as_str(),
+        )
+        .body(notification.body.unwrap_or_default().as_str())
+        .priority(match notification.priority {
+            NotificationPriority::Low => Priority::Low,
+            NotificationPriority::Normal => Priority::Normal,
+            NotificationPriority::High => Priority::High,
+            NotificationPriority::Urgent => Priority::Urgent,
+        });
+
+        for action in notification.actions {
+            let uuid = Uuid::new_v4();
+            let on_triggered = action.on_triggered.clone();
+            linux_notification_global.action_handlers.insert(
+                uuid,
+                Box::new(move |_, cx| {
+                    on_triggered.clone()(&NotificationActionEvent, cx);
+                }),
+            );
+            ashpd_notification = ashpd_notification
+                .button(Button::new(action.text.as_str(), uuid.to_string().as_str()));
+        }
+
+        ashpd_notification
     })
+}
+
+struct LinuxNotificationGlobal {
+    action_handlers: HashMap<Uuid, Box<dyn Fn(Option<String>, &mut App) + 'static>>,
+}
+
+impl Global for LinuxNotificationGlobal {}
+
+pub fn setup_linux_notifications(cx: &mut App) {
+    cx.set_global(LinuxNotificationGlobal {
+        action_handlers: HashMap::new(),
+    });
+
+    cx.spawn(async move |cx: &mut AsyncApp| {
+        // TODO: If the portal is not available, fall back to org.freedesktop.Notifications
+        let Ok(notification_portal) = NotificationProxy::new().await else {
+            return;
+        };
+
+        let Ok(mut action_invoked) = notification_portal.receive_action_invoked().await else {
+            return;
+        };
+
+        while let Some(action) = action_invoked.next().await {
+            if cx
+                .update_global::<LinuxNotificationGlobal, _>(|linux_notification_global, cx| {
+                    let Ok(uuid) = Uuid::try_parse(action.name()) else {
+                        return;
+                    };
+
+                    let Some(handler) = linux_notification_global.action_handlers.get(&uuid) else {
+                        return;
+                    };
+
+                    handler(None, cx);
+                })
+                .is_err()
+            {
+                return;
+            }
+        }
+    })
+    .detach();
 }
--- contemporary/src/platform_support/linux/setup.rs
+++ contemporary/src/platform_support/linux/setup.rs
@@ -1,11 +1,13 @@
 use crate::platform_support::linux::desktop_environment::DesktopEnvironment;
 use crate::platform_support::linux::gnome::setup::setup_gnome;
 use crate::platform_support::linux::kde::setup::setup_kde;
+use crate::platform_support::linux::notification::setup_linux_notifications;
 use crate::platform_support::linux::setup_xdg_portal_interface_theme_changed_listener::setup_xdg_portal_interface_theme_changed_listener;
 use gpui::App;
 
 pub fn setup_linux(cx: &mut App) {
     setup_xdg_portal_interface_theme_changed_listener(cx);
+    setup_linux_notifications(cx);
 
     match DesktopEnvironment::current() {
         Some(DesktopEnvironment::KDE) => setup_kde(cx),
