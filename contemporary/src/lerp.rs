420d29068a3d2df9cb0847d594753072ccb2970e
Victor Tran <vicr12345@gmail.com>
2026-03-08 17:04:34 +1100

    Implement lerp for bounds

--- contemporary/src/lerp.rs
+++ contemporary/src/lerp.rs
@@ -1,4 +1,5 @@
-use gpui::Rgba;
+use gpui::{Bounds, Pixels, Point, Rgba, Size};
+use std::fmt::Debug;
 
 pub trait Lerpable {
     fn lerp(&self, with: &Self, t: f32) -> Self;
@@ -26,5 +27,47 @@ impl Lerpable for Rgba {
             b: self.b.lerp(&with.b, t),
             a: self.a.lerp(&with.a, t),
         }
+    }
+}
+
+impl<T> Lerpable for Bounds<T>
+where
+    T: Clone + PartialEq + Default + Debug + Lerpable,
+{
+    fn lerp(&self, with: &Self, t: f32) -> Self {
+        Self {
+            origin: self.origin.lerp(&with.origin, t),
+            size: self.size.lerp(&with.size, t),
+        }
+    }
+}
+
+impl<T> Lerpable for Point<T>
+where
+    T: Clone + PartialEq + Default + Debug + Lerpable,
+{
+    fn lerp(&self, with: &Self, t: f32) -> Self {
+        Self {
+            x: self.x.lerp(&with.x, t),
+            y: self.y.lerp(&with.y, t),
+        }
+    }
+}
+
+impl<T> Lerpable for Size<T>
+where
+    T: Clone + PartialEq + Default + Debug + Lerpable,
+{
+    fn lerp(&self, with: &Self, t: f32) -> Self {
+        Self {
+            width: self.width.lerp(&with.width, t),
+            height: self.height.lerp(&with.height, t),
+        }
+    }
+}
+
+impl Lerpable for Pixels {
+    fn lerp(&self, with: &Self, t: f32) -> Self {
+        (*with - *self) * t + *self
     }
 }
