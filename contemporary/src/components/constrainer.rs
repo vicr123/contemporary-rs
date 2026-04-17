b67d2421ce745221ccba3672da4ab46f1a7b400f
Victor Tran <vicr12345@gmail.com>
2026-04-17 22:43:08 +1000

    Fix text in constrainer

--- contemporary/src/components/constrainer.rs
+++ contemporary/src/components/constrainer.rs
@@ -1,22 +1,31 @@
+use crate::components::anchorer::WithAnchorer;
+use gpui::prelude::FluentBuilder;
 use gpui::{
-    AnyElement, App, Div, ElementId, InteractiveElement, IntoElement, ParentElement, RenderOnce,
-    StatefulInteractiveElement, StyleRefinement, Styled, Window, div, px,
+    AnyElement, App, Bounds, Div, ElementId, InteractiveElement, IntoElement, ParentElement,
+    Pixels, RenderOnce, StatefulInteractiveElement, StyleRefinement, Styled, Window, div, px,
 };
 
 #[derive(IntoElement)]
 pub struct Constrainer {
     div: Div,
     id: ElementId,
+    width: Pixels,
 }
 
 pub fn constrainer(id: impl Into<ElementId>) -> Constrainer {
     Constrainer {
         div: div(),
         id: id.into(),
+        width: px(600.),
     }
 }
 
-impl Constrainer {}
+impl Constrainer {
+    pub fn inner_w(mut self, width: Pixels) -> Self {
+        self.width = width;
+        self
+    }
+}
 
 impl ParentElement for Constrainer {
     fn extend(&mut self, elements: impl IntoIterator<Item = AnyElement>) {
@@ -31,7 +40,9 @@ impl Styled for Constrainer {
 }
 
 impl RenderOnce for Constrainer {
-    fn render(self, _window: &mut Window, _cx: &mut App) -> impl IntoElement {
+    fn render(self, window: &mut Window, cx: &mut App) -> impl IntoElement {
+        let bounds = window.use_state(cx, |_, _| None::<Bounds<Pixels>>);
+
         div()
             .id(self.id)
             .flex()
@@ -39,6 +50,21 @@ impl RenderOnce for Constrainer {
             .items_center()
             .w_full()
             .overflow_y_scroll()
-            .child(self.div.max_w(px(600.)).w_full())
+            .child(
+                self.div.when_else(
+                    bounds
+                        .read(cx)
+                        .is_some_and(|bounds| bounds.size.width > self.width),
+                    |david| david.w(self.width),
+                    |david| david.max_w(self.width).w_full(),
+                ),
+            )
+            .with_anchorer({
+                let bounds_entity = bounds.clone();
+                move |david, bounds, _, cx| {
+                    bounds_entity.write(cx, Some(bounds));
+                    david
+                }
+            })
     }
 }
